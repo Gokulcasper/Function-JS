05951eb02f01b59e4f37ac7e40e23efc23d81b14
Gokulcasper <ronitcasper215@gmail.com>
2022-05-27 10:13:31 +0530

    Feat: Functions JS

---
+++ style.js
@@ -0,0 +1,11 @@
+function greetUser(Name1, Name2) {
+  let msg = "Hello " + Name1 + " , " + Name2 + " is a Developer";
+  console.log(msg);
+}
+function add(number1, number2) {
+  console.log(number1 + number2);
+}
+add(45, 55);
+
+greetUser("gokul", "raj");
+greetUser("mani", "sakthi");
